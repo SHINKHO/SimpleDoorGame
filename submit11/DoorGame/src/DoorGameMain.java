a86cecfda8eeade079f7aac088b05c468780b7d5
tlsru <tlsru@192.168.219.106>
2023-04-14 04:39:35 +0900

    done pointedString with box

--- submit11/DoorGame/src/DoorGameMain.java
+++ submit11/DoorGame/src/DoorGameMain.java
@@ -4,6 +4,8 @@ public class DoorGameMain {
 
 	public static void main(String[] args) {
 		// TODO 
+		GameControl gc = new GameControl(Map.getInstance());
+		/*테스트하던 라인
 		Map map = Map.getInstance();
 		map.addLocation(new Location(0,"테스트용",100, new int[] {2,2}, 100, 5, 10,30));
 		View aView = new View(new int[]{0,0},new int[] {100,100});
@@ -19,13 +21,8 @@ public class DoorGameMain {
 		Npc chair = new Npc(001,"의자","낡은 의자이다");
 		map.addNpc(chair,new int[] {0,0});
 		System.out.println(map.getInfoOf(new int[] {0,0}));
+		*/
 	}
 	
-//	public static void printLocInMap(Map map) {
-//		for(int i=0;i<map.getCopiedLocations().length;i++) {
-//			Location a= map.getLocationInstanceAt(map.getCopiedLocations()[i].getWhere());
-//			System.out.println("index" +i+" location at : "+System.identityHashCode(a)
-//			+"with object " +map.getLocationInstanceAt(map.getCopiedLocations()[i].getWhere()).getNpcStayIn().toString());
-//		}
-//	}
+
 }
