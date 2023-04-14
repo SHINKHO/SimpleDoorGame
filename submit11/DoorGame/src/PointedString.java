fd974114e1e0b275a2706e3bdea97f9c3a93918a
KYEONGHO SHIN <sinborninkr@gmail.com>
2023-04-14 16:49:31 +0900

    done

---
+++ submit11/DoorGame/src/PointedString.java
@@ -0,0 +1,18 @@
+
+public class PointedString {
+	private int[] point;
+	private String string;
+	public PointedString(int[] point, String string) {
+		super();
+		this.point = point;
+		this.string = string;
+	}
+	public int[] getPoint() {
+		return point;
+	}
+	public String getString() {
+		return string;
+	}
+	
+	
+}
